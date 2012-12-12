15b4943a6b95f954e5a7684e68f33817d6820190
Daniel Nussenbaum <daniel@server>
2012-12-12 18:35:46 -0500

    cleaning up project directory again

--- Archdeacon/src/gembed/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension("embeddingtest", sources=["embeddingtest.c", "gem.c", "gembed.c"])
-
-setup( name="PackageName",
-	version = "1.0",
-	description = "this is a demo package description",
-	ext_modules = [module1]
-	)
